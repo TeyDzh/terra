5748a2aa23ea7e5b0b3b5f2402714781ecac7384
tedzh <gamover644@gmail.com>
2023-05-26 01:33:31 +0300

    variables template

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region     = "eu-central-1"
+  region     = var.region
   profile    = "main"
 }
 
 resource "aws_vpc" "ecs_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "var.cidr_block"
   
   tags = {
     Name = "vpc"
@@ -13,7 +13,7 @@ resource "aws_vpc" "ecs_vpc" {
 
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.ecs_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = var.public_subnet
   map_public_ip_on_launch = true
   
   tags = {
@@ -23,7 +23,7 @@ resource "aws_subnet" "public_subnet" {
 
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = aws_vpc.ecs_vpc.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = var.public_cidr
   map_public_ip_on_launch = false
   
   tags = {
