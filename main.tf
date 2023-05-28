ea631c9e824587b4818ced39b763ea096ce18b07
TeyDzh <teymur.dzhafarov@matoffo.com>
2023-05-28 11:57:20 +0300

    file structure

--- main.tf
+++ main.tf
@@ -1,107 +1,20 @@
 provider "aws" {
-  region     = var.region
-  profile    = "main"
+  region  = var.region
+  profile = "main"
 }
 
-resource "aws_vpc" "ecs_vpc" {
-  cidr_block = "var.cidr_block"
-  
-  tags = {
-    Name = "vpc"
-  }
+module "vpc" {
+  source = "./vpc"
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.ecs_vpc.id
-  cidr_block              = var.public_subnet
-  map_public_ip_on_launch = true
-  
-  tags = {
-    Name = "public-subnet"
-  }
+module "ecs" {
+  source = "./ecs"
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.ecs_vpc.id
-  cidr_block              = var.public_cidr
-  map_public_ip_on_launch = false
-  
-  tags = {
-    Name = "private-subnet"
-  }
+module "alb" {
+  source = "./alb"
 }
 
-resource "aws_ecs_cluster" "main_cluster" {
-  name = "main-cluster"
+module "route_53" {
+  source = "./route_53"
 }
-
-resource "aws_lb" "alb" {
-  name               = "alb"
-  load_balancer_type = "application"
-  subnets            = [aws_subnet.public_subnet.id]
-
-  tags = {
-    Name = "alb"
-  }
-}
-
-resource "aws_ecs_task_definition" "task" {
-  family                = "task"
-  network_mode          = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  
-  container_definitions = <<DEFINITION
-  [
-    {
-      "name": "ДОДАТИ",
-      "image": "ДОДАТИ",
-      "portMappings": [
-        {
-          "containerPort": 80,
-          "hostPort": 80,
-          "protocol": "tcp"
-        }
-      ],
-      "cpu": 256,
-      "memory": 512
-    }
-  ]
-  DEFINITION
-}
-
-resource "aws_ecs_service" "service" {
-  name            = "service"
-  cluster         = "main-cluster"
-  task_definition = aws_ecs_task_definition.task.arn
-  desired_count   = 1
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.target_group.arn
-    container_name   = "container"
-    container_port   = 80
-  }
-}
-
-resource "aws_lb_target_group" "target_group" {
-  name     = "target-group"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.ecs_vpc.id
-
-  health_check {
-    path = "/"
-    port = 80
-  }
-}
-
-# resource "aws_route53_zone" "zone" {
-#   name = "domain.com"
-# }
-
-# resource "aws_route53_record" "my_record" {
-#   zone_id = aws_route53_zone.zone.zone_id
-#   name    = "domain.com"
-#   type    = "A"
-#   ttl     = "300"
-#   records = [aws_lb.my_alb.dns_name]
-# }
--- variables.tf
+++ variables.tf
@@ -2,13 +2,3 @@ variables "region" {
   default = "eu-central-1"
   type    = string
 }
-
-variables "cidr_block" {
-  default = "10.0.0.0/16"
-  type    = string
-}
-
-variable "public_cidr" {
-  default = "10.0.1.0/24"
-  type    = string
-}
